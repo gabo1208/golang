0f16fe1deaa62864160c0cb92c295f2f76819279
gabriel <gfreites@vmware.com>
2021-11-26 15:28:29 -0500

    added performance experiments for goroutines and started to create the source based topology

--- performance-experiments/main.go
+++ performance-experiments/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	start := time.Now()
+	workerCount := 1
+	wg := &sync.WaitGroup{}
+	wg.Add(workerCount)
+	workerQueue := make(chan map[int]int, workerCount)
+
+	for i := 0; i < workerCount; i++ {
+		go testFunc(wg, workerQueue)
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Test took %s", elapsed)
+	wg.Wait()
+}
+
+func testFunc(wg *sync.WaitGroup, c <-chan map[int]int) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		testRef(wg, c)
+	}
+}
+
+func testRef(wg *sync.WaitGroup, c <-chan map[int]int) {
+	i := 10
+	i *= i * i * i * i * i * i
 }
